6a97c525825ee701f796763261597e857ee79ae1
Leonilla Addeh <leonillaaddeh@macbookpro.myfiosgateway.com>
2026-05-23 17:06:31 -0400

    Fix teleprompter speed - human reading pace

--- pages/teleprompter.js
+++ pages/teleprompter.js
@@ -44,7 +44,7 @@ export default function Teleprompter() {
     let id
     const animate = () => {
       if (scrollRef.current) {
-        scrollRef.current.scrollTop += (speedRef.current * 1.2) + 1
+        scrollRef.current.scrollTop += speedRef.current * 0.15
         if (scrollRef.current.scrollTop + scrollRef.current.clientHeight >= scrollRef.current.scrollHeight) {
           setIsPlaying(false)
           return
