66b6a17d662e85bf212b329c0ddddb716b0f5489
nillad70-oss <nillad70@gmail.com>
2026-05-26 11:22:58 -0400

    Fix document error

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <meta name="application-name" content="NillaFlow Studio™" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content=content="NillaFlow Studio™" />
+        <meta name="apple-mobile-web-app-title" content="NillaFlow Studio™" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
         <link rel="icon" href="/favicon.ico" />
