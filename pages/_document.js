4926edb988e86150ec567dc8eda95b1b1fb329cb
nillad70-oss <nillad70@gmail.com>
2026-05-26 10:40:07 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,10 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#050505" />
-        <meta name="application-name" content="CreatorFlow Studio™" />
+        <meta name="application-name" content="NillaFlow Studio™" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="CreatorFlow Studio™" />
+        <meta name="apple-mobile-web-app-title" content=content="NillaFlow Studio™" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
         <link rel="icon" href="/favicon.ico" />
