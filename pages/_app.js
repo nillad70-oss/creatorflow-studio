fa2fac85c5e6863f5d6e25ba02edf8ffdd588d08
Leonilla Addeh <leonillaaddeh@macbookpro.myfiosgateway.com>
2026-06-07 23:56:56 -0400

    Add Meta Pixel to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        src="https://connect.facebook.net/en_US/fbevents.js"
+      />
+      <Script
+        id="meta-pixel-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            fbq('init', '983859764502251');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
